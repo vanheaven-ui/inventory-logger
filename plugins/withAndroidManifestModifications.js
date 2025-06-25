e4e3c25126109300bc8713f7a1d6aebabee31cdb
Mworekwa Ezekiel <vanheaven6@gmail.com>
2025-06-25 11:08:05 +0300

    Fix: Provide explicit value for appComponentFactory in manifest replacement

--- plugins/withAndroidManifestModifications.js
+++ plugins/withAndroidManifestModifications.js
@@ -1,40 +1,36 @@
 // plugins/withAndroidManifestModifications.js
-const { withAndroidManifest } = require("@expo/config-plugins");
+const { withAndroidManifest } = require('@expo/config-plugins');
 
 module.exports = function withAndroidManifestModifications(config) {
   return withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults;
 
     // Ensure the 'tools' namespace is declared in the manifest tag
-    // <manifest xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://schemas.android.com/tools">
-    if (!androidManifest.manifest.$["xmlns:tools"]) {
-      androidManifest.manifest.$["xmlns:tools"] =
-        "http://schemas.android.com/tools";
+    if (!androidManifest.manifest.$['xmlns:tools']) {
+      androidManifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
     }
 
     // Find the <application> tag
     const application = androidManifest.manifest.application[0];
 
-    // Add the tools:replace attribute to the application tag
-    // <application ... tools:replace="android:appComponentFactory">
     if (application.$) {
-      if (application.$["tools:replace"]) {
-        // If tools:replace already exists, append appComponentFactory to it
-        if (
-          !application.$["tools:replace"].includes(
-            "android:appComponentFactory"
-          )
-        ) {
-          application.$["tools:replace"] += ",android:appComponentFactory";
+      // Add the tools:replace attribute to the application tag
+      if (application.$['tools:replace']) {
+        if (!application.$['tools:replace'].includes('android:appComponentFactory')) {
+          application.$['tools:replace'] += ',android:appComponentFactory';
         }
       } else {
-        // If tools:replace doesn't exist, add it
-        application.$["tools:replace"] = "android:appComponentFactory";
+        application.$['tools:replace'] = 'android:appComponentFactory';
       }
+      // Explicitly set the appComponentFactory value to the AndroidX one
+      application.$['android:appComponentFactory'] = 'androidx.core.app.CoreComponentFactory';
+
     } else {
       // If no attributes exist on application tag, create the $ object
-
-      application.$ = { "tools:replace": "android:appComponentFactory" };
+      application.$ = {
+        'tools:replace': 'android:appComponentFactory',
+        'android:appComponentFactory': 'androidx.core.app.CoreComponentFactory'
+      };
     }
 
     return config;
