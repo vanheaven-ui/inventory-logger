13a8f5be00be9000427a64d215823b77c36ccaed
Mworekwa Ezekiel <vanheaven6@gmail.com>
2025-07-15 05:53:05 +0300

    fix:modify plugin

--- plugins/withAndroidExcludes.js
+++ plugins/withAndroidExcludes.js
@@ -2,7 +2,6 @@
 const { withAppBuildGradle } = require("@expo/config-plugins");
 
 function withAndroidExcludes(config) {
-  // Renamed function to match file name
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.language !== "groovy") {
       throw new Error(
@@ -13,80 +12,73 @@ function withAndroidExcludes(config) {
     let buildGradleContent = config.modResults.contents;
 
     // Define ALL packagingOptions content to be injected/merged
-    // Keep adding new 'pickFirst' rules here as new META-INF conflicts appear.
     const pickFirstRules = `
             pickFirst 'META-INF/androidx.localbroadcastmanager_localbroadcastmanager.version'
             pickFirst 'META-INF/androidx.customview_customview.version'
             pickFirst 'META-INF/androidx.legacy_legacy-support-core-ui.version'
             pickFirst 'META-INF/androidx.activity_activity.version'
             pickFirst 'META-INF/androidx.fragment_fragment.version'
-            pickFirst 'META-INF/androidx.legacy_legacy-support-core-utils.version' // <--- ADDED THIS CRITICAL LINE!
-            pickFirst 'META-INF/androidx.coordinatorlayout_coordinatorlayout.version' // Common additional one
-            pickFirst 'META-INF/androidx.drawerlayout_drawerlayout.version' // Common additional one
-            // Add more 'pickFirst' lines here as new META-INF conflicts appear.
-            // Example: pickFirst 'META-INF/another_conflicting_file.version'
+            pickFirst 'META-INF/androidx.legacy_legacy-support-core-utils.version'
+            pickFirst 'META-INF/androidx.coordinatorlayout_coordinatorlayout.version'
+            pickFirst 'META-INF/androidx.drawerlayout_drawerlayout.version'
     `;
 
-    // 1. Define the complete, desired packagingOptions block content
-    // This will be inserted or used to replace existing packagingOptions.
+    // 1. Construct the complete, desired packagingOptions block.
+    // We will always try to insert this exact block.
     const newPackagingOptionsBlock = `
-    // Expo Config Plugin - custom packagingOptions start
     packagingOptions {
         jniLibs {
             useLegacyPackaging (findProperty('expo.useLegacyPackaging')?.toBoolean() ?: false)
         }
         ${pickFirstRules}
     }
-    // Expo Config Plugin - custom packagingOptions end
-    `;
+    `; // No extra comments/markers here, to be cleaner
 
-    // 2. Remove ALL existing packagingOptions blocks first to prevent duplicates.
-    // This regex attempts to find any packagingOptions {...} block.
-    // The 's' flag allows '.' to match newlines. The '*' is non-greedy.
-    // We explicitly avoid matching the comment lines here to target just the Gradle block.
-    const packagingOptionsBlockRegex = /(packagingOptions\s*\{[^}]*?\})/gs;
-
-    let hasRemovedOldPackagingOptions = false;
-    buildGradleContent = buildGradleContent.replace(
-      packagingOptionsBlockRegex,
-      (match) => {
-        console.log(
-          "Expo Config Plugin: Removing an existing packagingOptions block for clean re-insertion."
-        );
-        hasRemovedOldPackagingOptions = true;
-        return ""; // Replace the matched block with an empty string
-      }
-    );
-    if (hasRemovedOldPackagingOptions) {
-      console.log(
-        "Expo Config Plugin: Successfully removed one or more packagingOptions blocks."
-      );
-    } else {
-      console.log(
-        "Expo Config Plugin: No existing packagingOptions blocks found to remove initially."
-      );
-    }
-
-    // 3. Find the 'android {' block and insert the consolidated packagingOptions
-    // This regex is crucial for identifying the correct insertion point without malforming the file.
-    // It tries to capture the entire 'android' block content, handling nested braces better.
+    // 2. Remove any existing packagingOptions block within the 'android' block.
+    // This is the safest way to ensure no duplicates or malformed existing blocks.
     const androidBlockRegex = /(android\s*\{[\s\S]*?\})/s;
+    let androidBlockMatch = buildGradleContent.match(androidBlockRegex);
 
-    if (buildGradleContent.match(androidBlockRegex)) {
+    if (androidBlockMatch) {
+      let androidBlockContent = androidBlockMatch[1]; // Get the full matched 'android { ... }' block including braces
+
+      // Remove any existing packagingOptions block from the content of the android block
+      const packagingOptionsInAndroidRegex = /packagingOptions\s*\{[\s\S]*?\}/g; // 'g' for global to catch multiple if any
+      let replacedCount = 0;
+      androidBlockContent = androidBlockContent.replace(
+        packagingOptionsInAndroidRegex,
+        (match) => {
+          console.log(
+            "Expo Config Plugin: Removing existing packagingOptions inside 'android' block."
+          );
+          replacedCount++;
+          return ""; // Remove it
+        }
+      );
+      if (replacedCount > 0) {
+        console.log(
+          `Expo Config Plugin: Removed ${replacedCount} existing packagingOptions blocks.`
+        );
+      } else {
+        console.log(
+          "Expo Config Plugin: No existing packagingOptions found inside 'android' block."
+        );
+      }
+
+      // Insert the newPackagingOptionsBlock just before the last '}' of the android block content.
+      const lastBraceIndex = androidBlockContent.lastIndexOf("}");
+      androidBlockContent =
+        androidBlockContent.substring(0, lastBraceIndex) +
+        `\n${newPackagingOptionsBlock}\n` + // Add newlines for formatting
+        androidBlockContent.substring(lastBraceIndex);
+
+      // Replace the old android block with the modified one in the full file content
       buildGradleContent = buildGradleContent.replace(
         androidBlockRegex,
-        (androidBlockMatch) => {
-          console.log(
-            "Expo Config Plugin: Injecting/replacing consolidated packagingOptions into 'android' block."
-          );
-          // Insert the new block just before the last '}' of the android block
-          const lastBraceIndex = androidBlockMatch.lastIndexOf("}");
-          return (
-            androidBlockMatch.substring(0, lastBraceIndex) +
-            `\n${newPackagingOptionsBlock}\n` + // Add newlines for formatting
-            androidBlockMatch.substring(lastBraceIndex)
-          );
-        }
+        androidBlockContent
+      );
+      console.log(
+        "Expo Config Plugin: Successfully injected new packagingOptions block into 'android' block."
       );
     } else {
       console.warn(
@@ -94,25 +86,25 @@ function withAndroidExcludes(config) {
       );
     }
 
-    // 4. Define the complete, desired configurations.all { exclude ... } content.
-    // This will be used to replace any existing configurations.all block.
+    // 3. Ensure configurations.all { exclude ... } block is present and contains all necessary excludes.
+    // This is the complete desired state for the configurations.all block.
     const requiredExcludeRules = `
     exclude group: 'com.android.support', module: 'support-compat'
     exclude group: 'com.android.support', module: 'versionedparcelable'
     exclude group: 'com.android.support', module: 'support-core-ui'
     exclude group: 'com.android.support', module: 'support-fragment'
     exclude group: 'com.android.support', module: 'support-activity'
-    exclude group: 'com.android.support', module: 'support-core-utils' // <--- ADDED THIS CRITICAL LINE!
-    exclude group: 'com.android.support', module: 'coordinatorlayout' // Corresponding to new pickFirst
-    exclude group: 'com.android.support', module: 'drawerlayout' // Corresponding to new pickFirst
+    exclude group: 'com.android.support', module: 'support-core-utils'
+    exclude group: 'com.android.support', module: 'coordinatorlayout'
+    exclude group: 'com.android.support', module: 'drawerlayout'
     `;
     const fullConfigurationsAllBlock = `
 configurations.all {
     ${requiredExcludeRules}
 }
-    `;
+`;
     // Remove any existing configurations.all block to replace it with our full version
-    const configurationsAllRegex = /(configurations\.all\s*\{[^}]*?\})/gs;
+    const configurationsAllRegex = /(configurations\.all\s*\{[\s\S]*?\})/gs;
     let hasRemovedConfigurationsAll = false;
     buildGradleContent = buildGradleContent.replace(
       configurationsAllRegex,
@@ -143,4 +135,4 @@ configurations.all {
   });
 }
 
-module.exports = withAndroidExcludes; 
+module.exports = withAndroidExcludes;
